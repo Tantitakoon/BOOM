24b11a022cd116fadeea1044f962b3adaef81df2
kilit <kilit130@infin.postbox.in.th>
2018-09-11 14:00:54 +0700

    c

--- src/app/messenger/process/index.js
+++ src/app/messenger/process/index.js
@@ -235,7 +235,7 @@ var process = exports.process = function (_Intent) {
                                             text: "OK, let's set your room preferences so I won't need to ask for them in the future.",
                                             buttons: [{
                                                 type: "web_url",
-                                                url: "https://blog.messengerdevelopers.com/using-the-webview-to-create-richer-bot-to-user-interactions-ed8a789523c6",
+                                                url: "https://chatbotboom.herokuapp.com/view",
                                                 title: "Set preferences",
                                                 webview_height_ratio: "compact",
                                                 messenger_extensions: false
