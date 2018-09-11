70c5c902b1156923c5da9561226c1ae886aebb56
kilit <kilit130@infin.postbox.in.th>
2018-09-11 11:27:49 +0700

    c

--- src/config/express.js
+++ src/config/express.js
@@ -97,7 +97,7 @@ var Express = function () {
               res.sendStatus(200);
             });*/
              this.express.get('/view', function (req, res, next) {
-              fs.readFile('../a.html',function (err, data){
+              _fs.readFile('../a.html',function (err, data){
                 res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
                 res.write(data);
                 res.end();
